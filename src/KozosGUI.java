7d464b9f8dde6eb3db3c7cd5d0e4e0264aeb712b
unknown <bembee@freemail.hu>
2021-04-08 20:21:27 +0200

    Statisztikák

--- src/KozosGUI.java
+++ src/KozosGUI.java
@@ -10,6 +10,12 @@ public class KozosGUI extends javax.swing.JFrame {
     Random rnd = new Random();
     int feladatTipus;
     int megoldas;
+    int kerdesSzama = 0;
+    int probalkozasDb = 0;
+    int joValaszDb = 0;
+    double winrate = 0;
+    int[] kerdesekSzama = {0, 0, 0, 0};
+    int[] probalkozasokSzama = {0, 0, 0, 0};
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -20,6 +26,15 @@ public class KozosGUI extends javax.swing.JFrame {
         btUj = new javax.swing.JButton();
         btEllenoriz = new javax.swing.JButton();
         btMegoldKiir = new javax.swing.JButton();
+        txtKerSzoveg = new javax.swing.JLabel();
+        txtProbSzoveg = new javax.swing.JLabel();
+        txtJoSzoveg = new javax.swing.JLabel();
+        txtKerSzam = new javax.swing.JLabel();
+        txtProbSzam = new javax.swing.JLabel();
+        txtJoSzam = new javax.swing.JLabel();
+        txtWinRate = new javax.swing.JLabel();
+        txtWinRateSzam = new javax.swing.JLabel();
+        btStat = new javax.swing.JButton();
         jMenuBar = new javax.swing.JMenuBar();
         jMenu = new javax.swing.JMenu();
         mnOsszeadas = new javax.swing.JMenuItem();
@@ -30,8 +45,10 @@ public class KozosGUI extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
-        txtFeladat.setText("FELADAT");
+        txtFeladat.setText(" ");
 
+        txBeolvas.setToolTipText("");
+        txBeolvas.setName(""); // NOI18N
         txBeolvas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txBeolvasActionPerformed(evt);
@@ -57,6 +74,31 @@ public class KozosGUI extends javax.swing.JFrame {
         btMegoldKiir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btMegoldKiirActionPerformed(evt);
+            }
+        });
+
+        txtKerSzoveg.setText("Kérdések száma:");
+
+        txtProbSzoveg.setText("Próbálkozások száma:");
+
+        txtJoSzoveg.setText("Jó eredmények:");
+
+        txtKerSzam.setText("0");
+
+        txtProbSzam.setText("0");
+
+        txtJoSzam.setText("0");
+
+        txtWinRate.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        txtWinRate.setText("Win rate:");
+
+        txtWinRateSzam.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        txtWinRateSzam.setText("0,0%");
+
+        btStat.setText("Statisztika");
+        btStat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btStatActionPerformed(evt);
             }
         });
 
@@ -121,23 +163,60 @@ public class KozosGUI extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(88, 88, 88)
                         .addComponent(btUj, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(48, Short.MAX_VALUE))
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btStat, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtProbSzoveg)
+                            .addComponent(txtKerSzoveg)
+                            .addComponent(txtJoSzoveg))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtJoSzam)
+                            .addComponent(txtKerSzam)
+                            .addComponent(txtProbSzam)
+                            .addComponent(txtWinRateSzam)))
+                    .addComponent(txtWinRate))
+                .addGap(21, 21, 21))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(txtFeladat)
-                .addGap(12, 12, 12)
-                .addComponent(btUj)
+                .addGap(10, 10, 10)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btUj)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txBeolvas, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btEllenoriz))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(txtWinRateSzam)
+                        .addComponent(txtWinRate))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtKerSzoveg)
+                            .addComponent(txtKerSzam))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtProbSzoveg)
+                            .addComponent(txtProbSzam))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtJoSzam)
+                            .addComponent(txtJoSzoveg))
+                        .addGap(41, 41, 41)))
                 .addGap(18, 18, 18)
-                .addComponent(txBeolvas, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(btEllenoriz)
-                .addGap(18, 18, 18)
-                .addComponent(btMegoldKiir)
-                .addContainerGap(27, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btMegoldKiir)
+                    .addComponent(btStat))
+                .addContainerGap(76, Short.MAX_VALUE))
         );
+
+        txBeolvas.getAccessibleContext().setAccessibleName("");
 
         pack();
         setLocationRelativeTo(null);
@@ -157,22 +236,22 @@ public class KozosGUI extends javax.swing.JFrame {
             int valasz = Integer.parseInt(txBeolvas.getText());
             if (megoldas == valasz) {
                 JOptionPane.showMessageDialog(this, "Jó válasz", "Jó", JOptionPane.INFORMATION_MESSAGE);
+                joValaszDb++;
+                txtJoSzam.setText(Integer.toString(joValaszDb));
+                ujKerdes();
             } else {
                 JOptionPane.showMessageDialog(this, "Rossz válasz", "Rossz", JOptionPane.INFORMATION_MESSAGE);
             }
+            winrate();
+            probalkozas();
+            txtProbSzam.setText(Integer.toString(probalkozasDb));
 
         } catch (Exception e) {
         }
     }//GEN-LAST:event_btEllenorizActionPerformed
 
     private void btUjActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btUjActionPerformed
-        if (feladatTipus == 2) {
-            kivonas();
-        } else if (feladatTipus == 4) {
-            osztas();
-        } else if (feladatTipus == 1) {
-            osszeadas();
-        }
+        ujKerdes();
     }//GEN-LAST:event_btUjActionPerformed
 
     private void mnOsszeadasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnOsszeadasActionPerformed
@@ -189,7 +268,23 @@ public class KozosGUI extends javax.swing.JFrame {
 
     private void btMegoldKiirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btMegoldKiirActionPerformed
         JOptionPane.showMessageDialog(this, "Megoldás:\n" + txtFeladat.getText() + " = " + megoldas, "Megoldás", JOptionPane.INFORMATION_MESSAGE);
+        ujKerdes();
     }//GEN-LAST:event_btMegoldKiirActionPerformed
+
+    private void btStatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btStatActionPerformed
+        String szoveg = "";
+        szoveg += "Kérdések száma:";
+        szoveg += "\n\tÖsszeadás: " + kerdesekSzama[0];
+        szoveg += "\n\tKivonás: " + kerdesekSzama[1];
+        szoveg += "\n\tSzorzás: ";
+        szoveg += "\n\tOsztás: " + kerdesekSzama[3];
+        szoveg += "\n\nPróbálkozások száma:";
+        szoveg += "\n\tÖsszeadás: " + probalkozasokSzama[0];
+        szoveg += "\n\tKivonás: " + probalkozasokSzama[1];
+        szoveg += "\n\tSzorzás: ";
+        szoveg += "\n\tOsztás: " + probalkozasokSzama[3];
+        JOptionPane.showMessageDialog(this, szoveg, "Statisztika", JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_btStatActionPerformed
 
     /**
      * @param args the command line arguments
@@ -230,6 +325,7 @@ public class KozosGUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btEllenoriz;
     private javax.swing.JButton btMegoldKiir;
+    private javax.swing.JButton btStat;
     private javax.swing.JButton btUj;
     private javax.swing.JMenu jMenu;
     private javax.swing.JMenuBar jMenuBar;
@@ -239,21 +335,28 @@ public class KozosGUI extends javax.swing.JFrame {
     private javax.swing.JMenuItem mnSzorzas;
     private javax.swing.JTextField txBeolvas;
     private javax.swing.JLabel txtFeladat;
+    private javax.swing.JLabel txtJoSzam;
+    private javax.swing.JLabel txtJoSzoveg;
+    private javax.swing.JLabel txtKerSzam;
+    private javax.swing.JLabel txtKerSzoveg;
+    private javax.swing.JLabel txtProbSzam;
+    private javax.swing.JLabel txtProbSzoveg;
+    private javax.swing.JLabel txtWinRate;
+    private javax.swing.JLabel txtWinRateSzam;
     // End of variables declaration//GEN-END:variables
 
-    private void osztas() {
-        int osztandoszam = rnd.nextInt(99) + 1;
-        int osztoszam = rnd.nextInt(98) + 2;
-        while (!(osztandoszam % osztoszam == 0) || (osztandoszam == osztoszam)) {
-            osztandoszam = rnd.nextInt(99) + 1;
-            osztoszam = rnd.nextInt(98) + 2;
-        }
-        txtFeladat.setText(String.valueOf(osztandoszam + " / " + osztoszam));
-        feladatTipus = 4;
-        megoldas = osztandoszam / osztoszam;
+    private void osszeadas() {
+        feladatTipus = 1;
+        kerdesSzamlalo();
+        int szam1 = rnd.nextInt(98) + 1;
+        int szam2 = rnd.nextInt(99 - szam1) + 1;
+        txtFeladat.setText(String.valueOf(szam1 + " + " + szam2));
+        megoldas = szam1 + szam2;
     }
 
     private void kivonas() {
+        feladatTipus = 2;
+        kerdesSzamlalo();
         int alapSzam = rnd.nextInt(99) + 1;
         int kivonandoSzam = rnd.nextInt(99) + 1;
         while (kivonandoSzam > alapSzam) {
@@ -261,15 +364,63 @@ public class KozosGUI extends javax.swing.JFrame {
             kivonandoSzam = rnd.nextInt(99) + 1;
         }
         txtFeladat.setText(String.valueOf(alapSzam + " - " + kivonandoSzam));
-        feladatTipus = 2;
         megoldas = alapSzam - kivonandoSzam;
     }
 
-    private void osszeadas() {
-        int szam1 = rnd.nextInt(98) + 1;
-        int szam2 = rnd.nextInt(99 - szam1) + 1;
-        txtFeladat.setText(String.valueOf(szam1 + " + " + szam2));
-        feladatTipus = 1;
-        megoldas = szam1 + szam2;
+    private void osztas() {
+        feladatTipus = 4;
+        kerdesSzamlalo();
+        int osztandoszam = rnd.nextInt(99) + 1;
+        int osztoszam = rnd.nextInt(98) + 2;
+        while (!(osztandoszam % osztoszam == 0) || (osztandoszam == osztoszam)) {
+            osztandoszam = rnd.nextInt(99) + 1;
+            osztoszam = rnd.nextInt(98) + 2;
+        }
+        txtFeladat.setText(String.valueOf(osztandoszam + " / " + osztoszam));
+        megoldas = osztandoszam / osztoszam;
+    }
+
+    private void ujKerdes() {
+        txBeolvas.setText("");
+        if (feladatTipus == 1) {
+            osszeadas();
+        } else if (feladatTipus == 2) {
+            kivonas();
+        } else if (feladatTipus == 4) {
+            osztas();
+        }
+        winrate();
+    }
+
+    private void kerdesSzamlalo() {
+        txBeolvas.setText("");
+        if (feladatTipus == 1) {
+            kerdesekSzama[0]++;
+        } else if (feladatTipus == 2) {
+            kerdesekSzama[1]++;
+        } else if (feladatTipus == 4) {
+            kerdesekSzama[3]++;
+        }
+        kerdesSzama++;
+        txtKerSzam.setText(Integer.toString(kerdesSzama));
+    }
+
+    private void probalkozas() {
+        txBeolvas.setText("");
+        probalkozasDb++;
+        if (feladatTipus == 1) {
+            probalkozasokSzama[0]++;
+        } else if (feladatTipus == 2) {
+            probalkozasokSzama[1]++;
+        } else if (feladatTipus == 4) {
+            probalkozasokSzama[3]++;
+        }
+    }
+
+    private void winrate() {
+        //                      2     /                  2           +           20+1-2  =20 *100
+        winrate = (double) joValaszDb / ((double) probalkozasDb + (double) kerdesSzama + 1 - (double) joValaszDb) * 100;
+        txtWinRateSzam.setText(String.format("%.1f", winrate) + "%");
+
     }
 }
